5bd48dda17afb5f394161db6d134352e732be228
D3v3l0per1 <manuel.widmoser12@gmail.com>
2020-02-06 21:26:11 +0100

    Authentication Middleware

--- raveweb-functions/functions/index.js
+++ raveweb-functions/functions/index.js
@@ -23,6 +23,7 @@ firebase.initializeApp(config);
 
 const db = admin.firestore();
 
+// Get all Posts
 app.get('/posts', (req, res) => {
   db.collection('posts')
     .orderBy('createdAt', 'desc')
@@ -40,10 +41,45 @@ app.get('/posts', (req, res) => {
     .catch(err => console.error(err));
 });
 
-app.post('/post', (req, res) => {
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer ')
+  ) {
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.error('No token found');
+    return res.status(403).json({ error: 'Unathorized' });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db
+        .collection('users')
+        .where('userId', '==', req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch(err => {
+      console.error('Error while verifying token', err);
+      return res.status(403).json(err);
+    });
+};
+
+// Create a Post
+app.post('/post', FBAuth, (req, res) => {
   const newPost = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString()
   };
 
@@ -145,7 +181,7 @@ app.post('/login', (req, res) => {
   if (isEmpty(user.email)) errors.email = 'Must not be empty';
   if (isEmpty(user.password)) errors.password = 'Must not be empty';
 
-  if (Object.keys(errors).length > 0) return res.status(400).json({errors});
+  if (Object.keys(errors).length > 0) return res.status(400).json({ errors });
 
   firebase
     .auth()
@@ -154,12 +190,14 @@ app.post('/login', (req, res) => {
       return data.user.getIdToken();
     })
     .then(token => {
-      return res.json({token});
+      return res.json({ token });
     })
     .catch(err => {
       console.error(err);
       if (err.code === 'auth/wrong-password') {
-        return res.status(403).json({ general: 'Wrong credentials, please try again' })
+        return res
+          .status(403)
+          .json({ general: 'Wrong credentials, please try again' });
       } else {
         return res.status(500).json({ error: error.code });
       }
