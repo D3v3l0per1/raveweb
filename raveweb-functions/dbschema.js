b48ca522fb2e66ccde08e74b7386a1622d7137fb
D3v3l0per1 <manuel.widmoser12@gmail.com>
2020-02-06 19:43:49 +0100

    Base Structure & Get Posts & Create Post

---
+++ raveweb-functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  posts: [
+    {
+      userHandle: 'user',
+      body: 'This is a post body',
+      createdAt: '2020-02-06T18:39:19.095Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
---
+++ raveweb-functions/functions/index.js
@@ -0,0 +1,56 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+const serviceAcc = require('./secret.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAcc),
+  databaseURL: 'https://raveweb-7ff67.firebaseio.com'
+});
+
+const express = require('express')
+const app = express()
+
+app.get('/posts', (req, res) => {
+  admin
+    .firestore()
+    .collection('posts')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then(data => {
+      let posts = [];
+      data.forEach(doc => {
+        posts.push({
+          postId: doc.id,
+          ...doc.data()
+        });
+      });
+      return res.json(posts);
+    })
+    .catch(err => console.error(err));
+})
+
+app.post('/post', (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(400).json({ error: 'Method not allowed' })
+  }
+  const newPost = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString()
+  };
+
+  admin
+    .firestore()
+    .collection('posts')
+    .add(newPost)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Somethin went wrong' });
+      console.error(err);
+    });
+});
+
+exports.api = functions.region('europe-west1').https.onRequest(app)
